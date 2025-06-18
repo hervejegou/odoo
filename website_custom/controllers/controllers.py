dcd0c7a1930c5cea4e224c7f22dd222cf6cf86e3
EslamTharwaat <eslam.tharwaat@gmail.com>
2025-06-19 00:08:18 +0200

    [ADD] website_custom module

--- website_custom/controllers/controllers.py
+++ website_custom/controllers/controllers.py
@@ -2,17 +2,21 @@
 from odoo import http
 from odoo.http import request
 import logging
+import json
 _logger = logging.getLogger(__name__)
 
 class IntercomUserData(http.Controller):
 
-    @http.route('/intercom/user_info', type='json', auth='public', csrf=False)
+    @http.route('/intercom/user_info', type='http', methods=['GET'], auth='public', csrf=False)
     def intercom_user_info(self):
         user = request.env.user
-        _logger.info(f"Fetching Intercom user info for user: {user.name} (ID: {user})")
-        return {
+        data= {
             'email': user.email,
             'user_id': user.id,
             'name': user.name,
         }
+        return http.Response(
+            json.dumps(data),
+            content_type='application/json; charset=utf-8'
+        )
 
