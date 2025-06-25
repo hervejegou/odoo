cf347184b752c5fef44ae994f8e19fa921531fcd
EslamTharwaat <eslam.tharwaat@gmail.com>
2025-06-25 19:57:25 +0200

    [ADD] auth0 logout logic

--- knk_auth0_sso/controllers/session_website.py
+++ knk_auth0_sso/controllers/session_website.py
@@ -11,8 +11,10 @@ class SessionWebsite(Session):
     @http.route('/web/session/logout', website=True, multilang=False, sitemap=False)
     def logout(self, redirect='/odoo'):
         auth0_provider = request.env['auth.oauth.provider'].sudo().search([('auth0_tenant_domain', '!=', False),('enabled','=', True)], limit=1)
-        if auth0_provider:
+        _logger.error("SESSSSSSSSSSSion: %s", request.session)
+        if auth0_provider and request.session:
             auth0_logout_url = auth0_provider.auth0_logout_url
+            request.session.destroy()
             return werkzeug_redirect(auth0_logout_url, code=303)
         return super().logout(redirect=redirect)
 
