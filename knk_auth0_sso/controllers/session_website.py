abebaaa9e8af83e7082f25caaa3357fa770a3739
EslamTharwaat <eslam.tharwaat@gmail.com>
2025-06-25 19:48:04 +0200

    [ADD] auth0 logout logic

--- knk_auth0_sso/controllers/session_website.py
+++ knk_auth0_sso/controllers/session_website.py
@@ -2,6 +2,7 @@ from odoo import http
 from odoo.http import request
 from odoo.addons.web.controllers.session import Session
 from werkzeug.utils import redirect as werkzeug_redirect
+import requests
 
 class SessionWebsite(Session):
 
@@ -9,6 +10,11 @@ class SessionWebsite(Session):
     def logout(self, redirect='/odoo'):
         auth0_provider = request.env['auth.oauth.provider'].sudo().search([('auth0_tenant_domain', '!=', False),('enabled','=', True)], limit=1)
         if auth0_provider:
-            redirect = auth0_provider.auth0_logout_url
-            return werkzeug_redirect(redirect, code=303)
+            auth0_logout_url = auth0_provider.auth0_logout_url
+            response = requests.request("GET", auth0_logout_url, headers={}, data={},timeout=10)
+            if response.status_code == 200:
+                return werkzeug_redirect(auth0_logout_url, code=303)
         return super().logout(redirect=redirect)
+
+
+
